9044a6d2e8fd9b7d57cc4b6cfcf6a7cc80867ce8
omar <umarfarooqa16@gmail.com>
2021-02-02 15:02:05 +0530

    ass4,ass5(not my codes)

---
+++ S4/DSA/Assignment_3/ASSG3_1.c
@@ -0,0 +1,299 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#define bool int 
+typedef struct node
+{
+	int data;
+	struct node *left,*right;
+	int ht;				
+}node;
+ 
+node *insert(node *,int);
+node *deleteNode(node *,int);
+int height( node *);
+node *rotateright(node *);
+node *rotateleft(node *);
+node *RR(node *);
+node *LL(node *);
+node *LR(node *);
+node *RL(node *);
+int BF(node *);
+int isAVL(node *);
+void printTree(node *);
+node* search(node *,int);
+int main()
+{
+	node *tree;
+	tree=NULL;
+	char a[50];
+	scanf("%s",a);
+	int k;
+	while(strcmp(a,"stop")!=0)
+	{
+		if(strcmp(a,"insr")==0)
+		{
+			scanf("%d",&k);
+			tree=insert(tree,k);
+		}
+		else if(strcmp(a,"srch")==0)
+		{
+			scanf("%d",&k);
+			if(search(tree,k)!=NULL)
+			{
+				printf("TRUE\n");
+			}
+			else 
+				printf("FALSE\n");
+		}
+		else if(strcmp(a,"delt")==0)
+                {
+			scanf("%d",&k);
+			tree=deleteNode(tree,k);
+                }
+		else if(strcmp(a,"disp")==0)
+		{
+			printTree(tree);
+			printf("\n");
+		}
+		else if(strcmp(a,"avlc")==0)
+		{
+			if(isAVL(tree)==1)
+				printf("TRUE\n");
+			else 
+				printf("FALSE\n");
+		}
+		else if(strcmp(a,"pbal")==0)
+		{
+			scanf("%d",&k);
+			printf("%d",BF(search(tree,k)));
+		}
+		else if(strcmp(a,"stop")==0)
+		{
+			break;
+		}
+			
+		scanf("%s",a);
+	}
+	return 0;
+
+}
+ 
+node * insert(node *T,int x)
+{
+	if(T==NULL)
+	{
+		T=(node*)malloc(sizeof(node));
+		T->data=x;
+		T->left=NULL;
+		T->right=NULL;
+	}
+	else
+		if(x > T->data)		
+		{
+			T->right=insert(T->right,x);
+			if(BF(T)==-2)
+				if(x>T->right->data)
+					T=RR(T);
+				else
+					T=RL(T);
+		}
+		else
+			if(x<T->data)
+			{
+				T->left=insert(T->left,x);
+				if(BF(T)==2)
+					if(x < T->left->data)
+						T=LL(T);
+					else
+						T=LR(T);
+			}
+		
+		T->ht=height(T);
+		
+		return(T);
+}
+ 
+node * deleteNode(node *T,int x)
+{
+	node *p;
+	
+	if(T==NULL)
+	{
+		return NULL;
+	}
+	else
+		if(x > T->data)		// insert in right subtree
+		{
+			T->right=deleteNode(T->right,x);
+			if(BF(T)==2)
+				if(BF(T->left)>=0)
+					T=LL(T);
+				else
+					T=LR(T);
+		}
+		else
+			if(x<T->data)
+			{
+				T->left=deleteNode(T->left,x);
+				if(BF(T)==-2)	//Rebalance during windup
+					if(BF(T->right)<=0)
+						T=RR(T);
+					else
+						T=RL(T);
+			}
+			else
+			{
+				//data to be deleted is found
+				if(T->right!=NULL)
+				{	//delete its inorder succesor
+					p=T->right;
+					
+					while(p->left!= NULL)
+						p=p->left;
+					
+					T->data=p->data;
+					T->right=deleteNode(T->right,p->data);
+					
+					if(BF(T)==2)//Rebalance during windup
+						if(BF(T->left)>=0)
+							T=LL(T);
+						else
+							T=LR(T);\
+				}
+				else
+					return(T->left);
+			}
+	T->ht=height(T);
+	return(T);
+}
+ 
+int height(node *T)
+{
+	int lh,rh;
+	if(T==NULL)
+		return(0);
+	
+	if(T->left==NULL)
+		lh=0;
+	else
+		lh=1+T->left->ht;
+		
+	if(T->right==NULL)
+		rh=0;
+	else
+		rh=1+T->right->ht;
+	
+	if(lh>rh)
+		return(lh);
+	
+	return(rh);
+}
+ 
+node * rotateright(node *x)
+{
+	node *y;
+	y=x->left;
+	x->left=y->right;
+	y->right=x;
+	x->ht=height(x);
+	y->ht=height(y);
+	return(y);
+}
+ 
+node * rotateleft(node *x)
+{
+	node *y;
+	y=x->right;
+	x->right=y->left;
+	y->left=x;
+	x->ht=height(x);
+	y->ht=height(y);
+	
+	return(y);
+}
+ 
+node * RR(node *T)
+{
+	T=rotateleft(T);
+	return(T);
+}
+ 
+node * LL(node *T)
+{
+	T=rotateright(T);
+	return(T);
+}
+ 
+node * LR(node *T)
+{
+	T->left=rotateleft(T->left);
+	T=rotateright(T);
+	
+	return(T);
+}
+ 
+node * RL(node *T)
+{
+	T->right=rotateright(T->right);
+	T=rotateleft(T);
+	return(T);
+}
+ 
+int BF(node *T)
+{
+	int lh,rh;
+	if(T==NULL)
+		return(0);
+ 
+	if(T->left==NULL)
+		lh=0;
+	else
+		lh=1+T->left->ht;
+ 
+	if(T->right==NULL)
+		rh=0;
+	else
+		rh=1+T->right->ht;
+ 
+	return(lh-rh);
+}
+bool isAVL(struct node* root) 
+{ 
+    int lh; 
+    int rh; 
+  
+    if (root == NULL) 
+        return 1; 
+  
+    lh = height(root->left); 
+    rh = height(root->right); 
+  
+    if (abs(lh - rh) <= 1 && isAVL(root->left) && isAVL(root->right)) 
+        return 1; 
+  
+    return 0; 
+} 
+void printTree(node* root)
+{
+	printf("( ");
+	if(root!=NULL)
+	{
+		printf("%d ",root->data);
+		printTree(root->left);
+		printTree(root->right);
+	}
+	printf(") ");	
+}
+node* search(node* root, int key)//normal search
+{
+	if (root == NULL || root->data == key)
+		return root;
+
+	if (root->data < key)
+		return search(root->right, key);
+
+	return search(root->left, key);
+	
+}
+
---
+++ S4/DSA/Assignment_3/ASSG3_5.c
@@ -0,0 +1,354 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct hashNODE
+{
+
+	int data;
+
+	int deleted;
+
+	int occupied;
+
+	struct hashNODE* link;
+};
+
+int m,m2;
+int c1,c2;
+
+struct hashNODE* hashARR = NULL;
+// create a hash table
+void hashTable(struct hashNODE **myArray)
+{
+	     *myArray = malloc(m * sizeof **myArray);
+}
+
+
+// linear probing
+int linear();
+int insertl();
+int searchl();
+int deletel();
+int print();
+
+//quadratic probing
+int quadratic();
+int insertq();
+int searchq();
+int deleteq();
+
+//double hashing
+int doubleh();
+int insertd();
+int searchd();
+int deleted();
+
+
+int main()
+{
+
+	char flag;
+	scanf("%s",&flag);
+
+	switch(flag)
+	{
+		case 'a': linear(); break;
+		case 'b': quadratic(); break;
+		case 'c': doubleh(); break;
+		default: return 0;
+	}
+	return 0;
+}
+
+//double hashing part
+int doubleh()
+{
+	int i;
+	scanf("%d",&m);
+	hashTable(&hashARR);
+	for(i=0;i<m;i++){
+		hashARR[i].occupied = 0;
+		hashARR[i].deleted = 0;
+	}
+	char op;
+	do 
+	{
+		scanf("%s",&op);
+		switch(op)
+		{
+
+			case 'i': insertd(); break;
+			case 's': searchd(); break;
+			case 'd': deleted(); break;
+			case 'p': print();break;
+			default: return 0;
+		}
+	} while(op!='T');
+}
+
+//inserting using double hashing
+int insertd()
+{
+	int data,key,flag=0;
+	scanf("%d",&data);
+	int keyM = data%m;
+	key = keyM;
+	int i,j;
+	for(i=m-1;i>1;i--)
+	{
+		for(j = 2; j <= i/2; ++j)
+		{
+			if(i % j == 0)
+			{
+				flag = 1;
+				break;
+			}
+		}
+		if (flag == 0)
+		{m2=i; break;}
+		flag=0;			//m2 is prime number just less than the table size ie m
+	}
+	int keyM2=m2-data%m2;	
+	i=0;
+	while(hashARR[key].occupied==1&&i<m)
+	{
+		key= (keyM+i*keyM2)%m;
+		i++;
+	}
+	hashARR[key].data=data;
+	hashARR[key].occupied=1;
+	hashARR[key].deleted=0;
+}
+
+// double hashing search
+int searchd()
+{
+	int data,key;
+	scanf("%d",&data);
+	int keyM = data%m;
+	key = keyM;
+	int keyM2=m2-data%m2;//equating hashes
+
+	int i=0;
+
+	while(hashARR[key].data!=data&&(hashARR[key].occupied==1||hashARR[key].deleted==1))
+	{
+			key= (keyM+i*keyM2)%m;
+				i++;
+					if(i==m) break;
+	}
+
+	if(hashARR[key].data==data) {printf("1\n"); return 1;}
+	else {printf("-1\n"); return -1;}
+}
+
+// double hashing deleting
+int deleted()
+{
+
+	int data,key;
+	scanf("%d",&data);
+	int keyM = data%m;
+	key = keyM;
+	int keyM2=m2-data%m2;
+	int i=0;
+
+	while(hashARR[key].data!=data&&(hashARR[key].occupied==1||hashARR[key].deleted==1))
+	{
+			key= (keyM+i*keyM2)%m;
+				i++;
+					if(i==m) break;
+	}
+
+	if(hashARR[key].data==data)
+	{
+			hashARR[key].data=0;
+			hashARR[key].occupied=0;
+			hashARR[key].deleted=1;
+	}
+	else return 0; 
+}
+
+// duadratic probing
+int quadratic()
+{
+	int i;	
+	scanf("%d",&m);
+	hashTable(&hashARR);
+	for(i=0;i<m;i++){
+		hashARR[i].occupied = 0;
+		hashARR[i].deleted = 0;
+	}
+	scanf("%d",&c1);
+	scanf("%d",&c2);
+	char op;
+	do 
+	{
+		scanf("%s",&op);
+		switch(op)
+		{
+			case 'i': insertq(); break;
+			case 's': searchq(); break;
+			case 'd': deleteq(); break;
+			case 'p': print();break;
+			default: return 0;
+		}
+	} while(op!='t');
+}
+
+//quadratic probing  inserting
+int insertq()
+{
+	int data,key;
+	scanf("%d",&data);
+	int keyM = data%m;
+	key = keyM;
+	int i=0;
+	while(hashARR[key].occupied==1&&i<m)
+	{       i++;
+		key= (keyM+i*c1+i*i*c2)%m;
+	}
+	hashARR[key].data=data;
+	hashARR[key].occupied=1;
+	hashARR[key].deleted=0;
+}
+//quadratic probing searchig
+int searchq()
+{
+	int data,key;
+	scanf("%d",&data);
+	int keyM = data%m;
+	key = keyM;
+	int i=0;
+	while(hashARR[key].data!=data&&(hashARR[key].occupied==1||hashARR[key].deleted==1))
+	{
+			key= (keyM+i*c1+i*i*c2)%m;
+			i++;
+			if(i==m) 
+				break;
+	}
+	if(hashARR[key].data==data) {printf("1\n"); return 1;}
+	else {printf("-1\n"); return -1;}
+}
+
+//deleteing quadratic probing
+int deleteq()
+{
+
+	int data,key;
+	scanf("%d",&data);
+	int keyM = data%m;
+	key = keyM;
+
+	int i=0;
+
+	while(hashARR[key].data!=data&&(hashARR[key].occupied==1||hashARR[key].deleted==1))
+	{
+			key= (keyM+i*c1+i*i*c2)%m;
+			i++;
+			if(i==m) break;
+	}
+
+	if(hashARR[key].data==data)
+	{
+
+		hashARR[key].data=0;
+		hashARR[key].occupied=0;
+		hashARR[key].deleted=1;
+	}
+	else 
+		return 0; 
+}
+
+// linear probing
+int linear()
+{
+	int i;
+	scanf("%d",&m);
+	hashTable(&hashARR);
+	for(i=0;i<m;i++){
+		hashARR[i].occupied = 0;
+		hashARR[i].deleted = 0;
+	}
+	char op;
+	do 
+	{
+		scanf("%s",&op);
+		switch(op)
+		{
+			case 'i': insertl(); break;
+			case 's': searchl(); break;
+			case 'd': deletel(); break;
+			case 'p': print();break;
+			default: return 0;
+		}
+		
+	} while(op!='T');
+}
+//linear inserting
+int insertl()
+{
+	int data,key;
+	scanf("%d",&data);
+	int keyM = data%m;
+	key = keyM;
+	int i=0;
+	while(hashARR[key].occupied==1)
+	{
+			key= (keyM+i)%m;
+				i++;
+	}
+	hashARR[key].data=data;
+	hashARR[key].occupied=1;
+	hashARR[key].deleted=0;
+}
+int searchl()
+{
+	int data,key;
+	scanf("%d",&data);
+	int keyM = data%m;
+	key = keyM;
+	int i=0;
+	while(hashARR[key].data!=data&&(hashARR[key].occupied==1||hashARR[key].deleted==1))
+	{
+		key= (keyM+i)%m;
+		i++;
+	}
+	if(hashARR[key].data==data) {printf("1\n"); return 1;}
+	else {printf("-1\n"); return -1;}
+}
+int deletel()
+{
+	int data,key;
+	scanf("%d",&data);
+	int keyM = data%m;
+	key = keyM;
+	int i=0;
+	while(hashARR[key].data!=data&&(hashARR[key].occupied==1||hashARR[key].deleted==1))
+	{
+		key= (keyM+i)%m;
+		i++;
+	}
+	if(hashARR[key].data==data)
+	{
+		hashARR[key].data=0;
+		hashARR[key].occupied=0;
+		hashARR[key].deleted=1;
+	}
+	else 
+		return 0; 
+}
+int print()
+{
+	int i;
+	for(i=0;i<m;i++)
+	{
+			if(hashARR[i].occupied==0)
+			{
+			       	printf("%d ()\n",i);
+			}
+			else 
+				printf("%d (%d)\n", i, hashARR[i].data);
+	}
+}
