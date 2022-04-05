d3855f646cff833a760daf254cd68bf55c3641c4
CodeWizard <codewizard001@gmail.com>
2022-04-05 15:29:13 +0530

    Completed In 1 file         - ECC         - Digital Signature using RSA

--- S8/IIS/DS_RSA.cpp
+++ S8/IIS/DS_RSA.cpp
@@ -2,10 +2,77 @@
 // Hash related functions are stored in crypto.h
 
 #include <NTL/ZZ.h>
-#include "crypto.h"
+#include <openssl/sha.h>
+#include <bits/stdc++.h>
+#include <string>
 
 using namespace std;
 using namespace NTL;
+
+char *hexdigest(unsigned char *md, int len)
+{
+    static char buf[80];
+    int i;
+    for (i = 0; i < len; i++)
+        sprintf(buf + i * 2, "%02x", md[i]);
+    return buf;
+}
+
+ZZ hexToZZ(char *hex)
+{
+   ZZ res = ZZ(0);
+   int i;
+   for (i = 0; i < strlen(hex); i += 1)
+   {
+      res <<= 4;
+      char x = hex[i];
+      if(x>='0' && x <='9')
+         res += hex[i]-48;
+      else if(x>='a' && x <='f')
+         res += hex[i]-87;
+      else if(x>='A' && x <='F')
+         res += hex[i]-55;
+   }
+   return res ;
+}
+
+string numberToString(ZZ num)
+{
+    string s = "";
+    while (num > 0)
+    {
+        s += (num % 10) + '0';
+        num /= 10;
+    }
+    reverse(s.begin(), s.end());
+    return s;
+}
+
+// SHA1
+ZZ Hash(string s)
+{
+    unsigned char hash[SHA_DIGEST_LENGTH]; // == 20
+
+    SHA_CTX sha1;
+    SHA1_Init(&sha1);
+    SHA1_Update(&sha1, s.c_str(), s.length());
+    SHA1_Final(hash, &sha1);
+
+    ZZ h = hexToZZ(hexdigest(hash, SHA_DIGEST_LENGTH));
+    return h ;
+}
+
+ZZ Hash(ZZ m)
+{
+    string s = numberToString(m);
+    return Hash(s);
+}
+
+ZZ Hash(char *m)
+{
+    string s = m;
+    return Hash(s);
+}
 
 #define BIT_LENGTH 512
 #define ERR_THRESHOLD 1000 
@@ -89,7 +156,7 @@ int main()
    cout << "--------------------------------------------------------------------------------------" << endl ;
    
    bool t1 = Verify(m,sigma + 1,e,n) ;
-   cout << "Pass 2" << endl ;
+   cout << "Pass 2 - [With Tampering]" << endl ;
    cout << "Message : " << m << endl ;
    cout << "Sign    : " << sigma + 1 << endl ;
    cout << "Valid   : " << t1 << endl ;
